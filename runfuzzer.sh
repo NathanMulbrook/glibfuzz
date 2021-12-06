3f9787f7a1ef500a37cc33e3ce0fc214ef8a3dac
Nathan Mulbrook <nathanmulbrook@gmail.com>
2021-12-05 23:13:33 -0600

    support multiple fuzz
    Signed-off-by: Nathan Mulbrook: <nathanmulbrook@gmail.com>

--- runfuzzer.sh
+++ runfuzzer.sh
@@ -1,5 +1,35 @@
-export AFL_QEMU_PERSISTENT_ADDR=0x$(nm fuzz_cos | grep "T LLVMFuzzerTestOneInput" | awk '{print $1}')
-export AFL_QEMU_PERSISTENT_HOOK=/home/admin/software/fuzzing/AFLplusplus/utils/aflpp_driver/aflpp_qemu_driver_hook.so
+#!/bin/bash
+_term() {
+    pkill -P $$
+}
 
 
-AFL_QEMU_PERSISTENT_GPR=1 AFL_QEMU_PERSISTENT_MEM=1 AFL_FORKSRV_INIT_TMOUT=60000  AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 /home/admin/software/fuzzing/AFLplusplus/afl-fuzz -Q -i input -o output ./fuzz_cos
+trap _term SIGTERM
+
+builds=$(pwd)/build.sh
+fuzzer=$1
+if [ -f "$builds" ]
+then
+    cd build/run/
+    echo "Moving to Run Dir"
+    ./runfuzzer.sh $fuzzer
+else
+    if [ -z ${fuzzer} ]
+    then
+        for dir in */; do
+                    echo "----Launching ${dir::-1}"
+
+            ./runfuzzer.sh ${dir::-1} &
+        done
+    else
+
+            cd $fuzzer
+            echo "--------------Launching $fuzzer"
+            pwd
+            export AFL_QEMU_PERSISTENT_ADDR=0x$(nm $fuzzer | grep "T LLVMFuzzerTestOneInput" | awk '{print $1}')
+            export AFL_QEMU_PERSISTENT_HOOK=/home/admin/software/fuzzing/AFLplusplus/utils/aflpp_driver/aflpp_qemu_driver_hook.so
+
+
+            AFL_QEMU_PERSISTENT_GPR=1 AFL_QEMU_PERSISTENT_MEM=1 AFL_FORKSRV_INIT_TMOUT=60000  AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 /home/admin/software/fuzzing/AFLplusplus/afl-fuzz -Q -i input -o output ./$fuzzer
+    fi
+fi
