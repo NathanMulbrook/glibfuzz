0f0fab7aff14d870d8e04de56af48eaea21d777d
Matthew Burket <m@tthewburket.com>
2021-12-06 00:00:33 -0600

    Fix AFL_ROOT in runfuzzer.sh

--- runfuzzer.sh
+++ runfuzzer.sh
@@ -30,9 +30,9 @@ else
             echo "--------------Launching $fuzzer"
             pwd
             export AFL_QEMU_PERSISTENT_ADDR=0x$(nm $fuzzer | grep "T LLVMFuzzerTestOneInput" | awk '{print $1}')
-            export AFL_QEMU_PERSISTENT_HOOK=/home/admin/software/fuzzing/AFLplusplus/utils/aflpp_driver/aflpp_qemu_driver_hook.so
+            export AFL_QEMU_PERSISTENT_HOOK=$AFL_ROOT/utils/aflpp_driver/aflpp_qemu_driver_hook.so
 
 
-            AFL_QEMU_PERSISTENT_GPR=1 AFL_QEMU_PERSISTENT_MEM=1 AFL_FORKSRV_INIT_TMOUT=60000  AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 /home/admin/software/fuzzing/AFLplusplus/afl-fuzz -Q -i input -o output ./$fuzzer
+            AFL_QEMU_PERSISTENT_GPR=1 AFL_QEMU_PERSISTENT_MEM=1 AFL_FORKSRV_INIT_TMOUT=60000  AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 $AFL_ROOT/afl-fuzz -Q -i input -o output ./$fuzzer
     fi
 fi
